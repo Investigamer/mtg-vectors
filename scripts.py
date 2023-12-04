6380b423723ed204b5638f4ef268b0de65786991
Investigamer <freethoughtleft@gmail.com>
2023-12-03 22:17:55 -0600

    refactor(data): Rework symbol map naming to match Scryfall, move alternates to .alt directory
    Signed-off-by: Investigamer <freethoughtleft@gmail.com>

--- scripts.py
+++ scripts.py
@@ -6,24 +6,42 @@
 import os
 from pathlib import Path
 from pprint import pprint
-from typing import Union
+from typing import Union, Optional, TypedDict
 
 # Third Party Imports
 import requests
 import yaml
+import yarl
 
 # Empty data objects
-ROUTES_SET = {}
-MISSING_SET = {}
+CORRECTED_SET = {}
+EMPTY_SET = []
 IGNORED_SET = []
+MISSING_SET = []
+MIXED_SET = []
+ROUTES_SET = {}
 
 # Paths
 PATH_ROOT = Path(os.getcwd())
 PATH_SET = PATH_ROOT / 'svg' / 'set'
 PATH_DATA = PATH_ROOT / 'data'
-PATH_ROUTES_SET = PATH_DATA / 'routes.set.yml'
-PATH_IGNORE_SET = PATH_DATA / 'ignored.set.yml'
+PATH_ALIAS_SET = PATH_DATA / 'alias.set.yml'
+PATH_CORRECTED_SET = PATH_DATA / 'corrected.set.yml'
+PATH_EMPTY_SET = PATH_DATA / 'empty.set.yml'
+PATH_IGNORED_SET = PATH_DATA / 'ignored.set.yml'
 PATH_MISSING_SET = PATH_DATA / 'missing.set.yml'
+PATH_MIXED_SET = PATH_DATA / 'mixed.set.yml'
+PATH_ROUTES_SET = PATH_DATA / 'routes.set.yml'
+
+
+class SetDetails(TypedDict):
+    """Set details dictionary."""
+    type: str
+    code: str
+    parent: Optional[str]
+    icon: str
+    name: str
+
 
 """
 * UTIL Funcs
@@ -48,7 +66,7 @@ def load_data_file(path: Path) -> Union[list, dict]:
 """
 
 
-def get_all_sets() -> list[dict]:
+def get_all_sets() -> list[SetDetails]:
     """Make a GET request to the https://api.scryfall.com/sets endpoint.
 
     Returns:
@@ -140,14 +158,14 @@ def get_missing_vectors_set() -> tuple[dict[str, list[str]], dict[str, dict[str,
 
 def get_unused_vectors_set() -> set[str]:
     """Get a set (non-repeating list) of symbols in the repository not used by any known Scryfall 'Set' object."""
-    sets = set([n['code'] for n in get_all_sets()])
+    sets = set([yarl.URL(n['icon']).with_suffix('').name.upper() for n in get_all_sets()])
     return {
         n for n in os.listdir(PATH_SET)
-        if not n.lower() in sets and n not in IGNORED_SET
+        if n not in sets and n not in IGNORED_SET
     }
 
 
-def get_sets_by_symbol(sym: str) -> list[dict]:
+def get_sets_by_symbol(sym: str) -> list[SetDetails]:
     """Return a list of set codes that use a given SVG symbol.
 
     Args:
@@ -185,6 +203,92 @@ def analyze_missing_vectors_set() -> None:
         print("NO SETS MISSING!")
 
 
+def get_symbol_code(code: str, parent: Optional[str]) -> Optional[str]:
+    """Look for the system appropriate symbol code for a given set code with optional parent code.
+
+    Args:
+        code: 3-5 letter set code.
+        parent: 3-5 letter set code of the parent set, if required.
+
+    Returns:
+        Best guess at the appropriate matching symbol code based on the available data files.
+    """
+
+    # Replace problematic windows names
+    if code == 'CON':
+        return 'conf'
+
+    # Skip this if it's in the reroutes
+    if code in ROUTES_SET:
+        return ROUTES_SET[code]
+
+    # Skip this set if we have it in the SVG library
+    if Path(PATH_SET, code).is_dir():
+        return code
+
+    # Skip if set routes to a parent present in the SVG library
+    if parent and Path(PATH_SET, parent).is_dir():
+        return parent
+
+    # Skip if set's parent is present in the reroutes
+    if parent and parent in ROUTES_SET:
+        return ROUTES_SET[parent]
+
+    # Promo sets with parent
+    if len(code) > 3 and parent:
+
+        # Skip promo sets with known parent, e.g. pMID -> MID
+        short = code[1:]
+        if short == 'CON':
+            return 'conf'
+        if parent == short:
+            return get_symbol_code(short, None)
+
+        # Skip promo token sets with known parent, e.g. ptSNC -> SNC
+        short = code[2:]
+        if short == 'CON':
+            return 'conf'
+        if parent[1:] == short:
+            return get_symbol_code(short, None)
+
+    # Default to code
+    return code
+
+
+def compare_sets_and_symbols() -> list[tuple[str, Optional[str], str]]:
+    """Checks the symbol icon of every set against the current defined symbol code for that set."""
+    results = []
+    sets = get_all_sets()
+    for data in sets:
+
+        # Skip sets with no symbol or mixed symbols
+        if data['code'].upper() in EMPTY_SET or data['code'].upper() in MIXED_SET:
+            continue
+
+        # Skip irrelevant non-playable cards
+        if data['type'] in ['minigame', 'token']:
+            continue
+        if data['type'] == 'memorabilia' and 'Front Cards' in data['name']:
+            continue
+
+        # Get the Scryfall appropriate symbol name
+        symbol = yarl.URL(data['icon']).with_suffix('').name.lower()
+        if data['code'].upper() in CORRECTED_SET:
+            symbol = CORRECTED_SET.get(data['code'].upper()).lower()
+
+        # Get the currently established symbol name
+        current = get_symbol_code(data['code'].upper(), data['parent'].upper() if data['parent'] else None).lower()
+        if current == 'conf':
+            continue
+        if symbol.upper() in ALIAS_SET.get(current.upper(), []):
+            continue
+
+        # Established symbol name doesn't match the Scryfall name
+        if symbol != current:
+            results.append((data['code'], current, symbol))
+    return results
+
+
 """
 * Launch Script
 """
@@ -192,9 +296,25 @@ def analyze_missing_vectors_set() -> None:
 if __name__ == '__main__':
 
     # Load our data
+    ALIAS_SET = load_data_file(PATH_ALIAS_SET)
+    CORRECTED_SET = load_data_file(PATH_CORRECTED_SET)
+    EMPTY_SET = load_data_file(PATH_EMPTY_SET).get('empty', [])
+    IGNORED_SET = load_data_file(PATH_IGNORED_SET).get('ignored', [])
+    MISSING_SET = load_data_file(PATH_MISSING_SET)
+    MIXED_SET = load_data_file(PATH_MIXED_SET).get('mixed', [])
     ROUTES_SET = load_data_file(PATH_ROUTES_SET)
-    IGNORED_SET = load_data_file(PATH_IGNORE_SET)
-    MISSING_SET = load_data_file(PATH_MISSING_SET).get('missing', [])
+
+    # Check misnamed symbol codes
+    print("MISNAMED SYMBOLS:")
+    print("=" * 120)
+    pprint(compare_sets_and_symbols())
+    print("=" * 120)
+
+    # Get unused set symbols
+    print("UNUSED SYMBOLS:")
+    print("=" * 120)
+    pprint(get_unused_vectors_set())
+    print("=" * 120)
 
     # Analyze missing sets
     analyze_missing_vectors_set()
